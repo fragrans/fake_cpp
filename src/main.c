63649fdc0ba963ff0cda7178ae2626aecb423c3b
lll <lll@xeltek-cn.com>
2026-07-29 02:40:43 +0000

    add presets

--- src/main.c
+++ src/main.c
@@ -1,3 +1,11 @@
+#include <stdio.h>
+#include <sqlite3.h>
+#include <yaml-cpp/yaml.h>
+
 int main(void){
+    printf("Hello world\r\n");
+    sqlite3 *s_db = 0;
+    sqlite3_open(":memory:", &s_db);
+    sqlite3_close(s_db);
     return 0;
 }
