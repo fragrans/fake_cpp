718f6ae5665954d96539cf483542bd2c31035fbc
LL L <lll@xeltek-cn.comn>
2026-07-30 09:36:58 +0800

    update cache.

--- src/main.c
+++ src/main.c
@@ -1,11 +1,10 @@
-#include <stdio.h>
 #include <sqlite3.h>
+#include <stdio.h>
 
-int main(void){
-    
-    sqlite3 *s_db = 0;
-    sqlite3_open(":memory:", &s_db);
-    printf("db: %p\r\n", s_db);
-    sqlite3_close(s_db);
-    return 0;
+int main(void) {
+  sqlite3 *s_db = 0;
+  sqlite3_open(":memory:", &s_db);
+  printf("db: %p\r\n", s_db);
+  sqlite3_close(s_db);
+  return 0;
 }
